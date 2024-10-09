3af89752d3c0525604f4fdc9c55f3d936170f0ea
Geisbelly <geisbelly19@gmail.com>
2024-10-09 00:05:56 -0300

    NC

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,35 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#1a73e8",   
+        secondary: "#e37400",
+        tercedary: "#033980", 
+        background: "#f5f5f5", 
+        foreground: "#333333",
+        fundo: "#e6f1ff",
       },
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+      },
+      spacing: {
+        '72': '18rem',  // Exemplo de espaçamento personalizado
+        '84': '21rem',
+        '96': '24rem',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],  // Fonte sem serifa
+        serif: ['Georgia', 'Times', 'serif'],                 // Fonte com serifa
+      },
+
     },
   },
   plugins: [],
